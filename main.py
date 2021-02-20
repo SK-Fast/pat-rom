e091e5dffea81a2f1a91a55b70b4083500d07274
SK-Fast <60780448+SK-Fast@users.noreply.github.com>
2021-02-20 11:07:14 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
 def on_button_pressed_a():
     pass
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_ir_button_pressed():
+    global switch_
+    serial.write_value("x", 0)
+    if makerbit.ir_button() == 160:
+        switch_ = 1
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        basic.show_leds("""
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            """)
+    elif makerbit.ir_button() == 80:
+        switch_ = 2
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P0, 1)
+        basic.show_leds("""
+            . # # # .
+            # . . . #
+            # . . . #
+            . . # . #
+            . # # # #
+            """)
+    elif True:
+        pass
+makerbit.on_ir_button(IrButton.ANY, IrButtonAction.PRESSED, on_ir_button_pressed)
+
+switch_ = 0
+switch_ = 1
+makerbit.connect_ir_receiver(DigitalPin.P2, IrProtocol.KEYESTUDIO)
+basic.show_leds("""
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    """)
--- main.ts
+++ main.ts
@@ -1,3 +1,50 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
+	
 })
+makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
+    serial.writeValue("x", 0)
+    if (makerbit.irButton() == 160) {
+        switch_ = 1
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    } else if (makerbit.irButton() == 80) {
+        switch_ = 2
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . . . #
+            . . # . #
+            . # # # #
+            `)
+    } else if (makerbit.irButton() == 16) {
+        switch_ = 3
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . . . #
+            # . # . .
+            . # # # .
+            `)
+    }
+})
+let switch_ = 0
+switch_ = 1
+makerbit.connectIrReceiver(DigitalPin.P2, IrProtocol.Keyestudio)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
